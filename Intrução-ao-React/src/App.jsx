390f0bad1bc323842aef7405841ec4c0bcedb078
Matheus Bortolotto <matheusbortolol@hotmail.com>
2024-08-16 22:43:14 -0300

    Introdução de como usar o React
    Introdução de como usar o React com o vite

---
+++ Intrução-ao-React/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './componentes/Header'
+
+function App() {
+
+  return (
+    <>
+     <input className='' type="text" name='' id='' />
+      <Header/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ Intrução-ao-React/src/componentes/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+
+    return(
+        <>
+            <h1>Titulo da pagina</h1>
+            <p>Test</p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </>
+    )
+}
+
+export default Header
